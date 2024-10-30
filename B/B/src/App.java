920340501b0a54e5e7017a0c6a3366dbf456e7e7
rioleo2 <rioleo2@mail.ru>
2024-10-30 21:41:00 +0300

    Оптимизировал задачу B, но по времени в тестовой системе все равно не проходит

--- B/B/src/App.java
+++ B/B/src/App.java
@@ -1,13 +1,49 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
+
+    public static void quickSort(String[][] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    // Разделение массива для быстрой сортировки
+    public static int partition(String[][] arr, int low, int high) {
+        String[] pivot = arr[high];
+        int pivotId = Integer.parseInt(pivot[3]);
+        int pivotTime = Integer.parseInt(pivot[5]);
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            int currentId = Integer.parseInt(arr[j][3]);
+            int currentTime = Integer.parseInt(arr[j][5]);
+
+            if (currentId < pivotId || (currentId == pivotId && currentTime < pivotTime)) {
+                i++;
+                String[] temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        String[] temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int N;
 
-        System.out.println("Введите количество строк логов: ");
+        // System.out.println("Введите количество строк логов: ");
 
         N = scanner.nextInt(); // N = количество записей в логе
 
@@ -17,7 +53,7 @@ public class App {
         int countOfMinutes[] = new int[N];
 
         scanner.nextLine();
-        System.out.println("Введите логи: ");
+        // System.out.println("Введите логи: ");
         for (int i = 0; i < N; i++) {
             log[i] = scanner.nextLine(); // запись в массив строк лога
             LogData[i] = log[i].split(" ");
@@ -37,93 +73,76 @@ public class App {
             int doyToMinutes = Integer.parseInt(LogData[i][0]) * 60 * 24;
             int hoursToMinutes = Integer.parseInt(LogData[i][1]) * 60;
             int minutesToMinutes = Integer.parseInt(LogData[i][2]);
-            LogData[i][5] = Integer.toString(doyToMinutes + hoursToMinutes + minutesToMinutes);
+            int totalMinutes = doyToMinutes + hoursToMinutes + minutesToMinutes;
+
+            LogData[i][5] = Integer.toString(totalMinutes);
         }
 
-        System.out.println("\nВы ввели: ");
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < 6; j++) {
-                System.out.print(LogData[i][j] + " ");
-            }
-            System.out.println();
-        }
+        // Сортировка массива LogData по LogData[i][3] и LogData[i][5]
+        quickSort(LogData, 0, N - 1);
 
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if (Integer.parseInt(LogData[j][3]) < Integer.parseInt(LogData[i][3])) {
-                    String[] temp = LogData[i];
-                    LogData[i] = LogData[j];
-                    LogData[j] = temp;
-                }
-            }
-        }
+        // System.out.println("\nВы ввели: ");
+        // for (int i = 0; i < N; i++) {
+        // for (int j = 0; j < 6; j++) {
+        // System.out.print(LogData[i][j] + " ");
+        // }
+        // System.out.println();
+        // }
 
-        System.out.println("\nМассив после сортировки по ID: ");
+        // for (int i = 0; i < N - 1; i++) {
+        // for (int j = i + 1; j < N; j++) {
+        // if (Integer.parseInt(LogData[j][3]) < Integer.parseInt(LogData[i][3])) {
+        // String[] temp = LogData[i];
+        // LogData[i] = LogData[j];
+        // LogData[j] = temp;
+        // }
+        // }
+        // }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < 6; j++) {
-                System.out.print(LogData[i][j] + " ");
-            }
-            System.out.println();
-        }
+        Arrays.sort(LogData, (a, b) -> Integer.parseInt(a[3]) - Integer.parseInt(b[3]));
 
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if (Integer.parseInt(LogData[j][3]) == Integer.parseInt(LogData[i][3])) {
-                    if (Integer.parseInt(LogData[j][0]) < Integer.parseInt(LogData[i][0])) {
-                        String[] temp = LogData[i];
-                        LogData[i] = LogData[j];
-                        LogData[j] = temp;
-                    }
-                }
-            }
-        }
+        // System.out.println("\nМассив после сортировки по ID: ");
 
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if (Integer.parseInt(LogData[j][3]) == Integer.parseInt(LogData[i][3])) {
-                    if ((Integer.parseInt(LogData[j][1]) < Integer.parseInt(LogData[i][1]))
-                            && (Integer.parseInt(LogData[j][0]) == Integer.parseInt(LogData[i][0]))) {
-                        String[] temp = LogData[i];
-                        LogData[i] = LogData[j];
-                        LogData[j] = temp;
-                    }
-                }
-            }
-        }
+        // for (int i = 0; i < N; i++) {
+        // for (int j = 0; j < 6; j++) {
+        // System.out.print(LogData[i][j] + " ");
+        // }
+        // System.out.println();
+        // }
 
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if (Integer.parseInt(LogData[j][3]) == Integer.parseInt(LogData[i][3])) {
-                    if ((Integer.parseInt(LogData[j][2]) < Integer.parseInt(LogData[i][2]))
-                            && (Integer.parseInt(LogData[j][1]) == Integer.parseInt(LogData[i][1]))) {
-                        String[] temp = LogData[i];
-                        LogData[i] = LogData[j];
-                        LogData[j] = temp;
-                    }
-                }
-            }
-        }
+        // for (int i = 0; i < N - 1; i++) {
+        //     for (int j = i + 1; j < N; j++) {
+        //         if (Integer.parseInt(LogData[j][3]) == Integer.parseInt(LogData[i][3])) {
+        //             if (Integer.parseInt(LogData[j][5]) < Integer.parseInt(LogData[i][5])) {
+        //                 String[] temp = LogData[i];
+        //                 LogData[i] = LogData[j];
+        //                 LogData[j] = temp;
+        //             }
+        //         }
+        //     }
+        // }
 
-        System.out.println("\nМассив после сортировки по времени: ");
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < 6; j++) {
-                System.out.print(LogData[i][j] + " ");
-            }
-            System.out.println();
-        }
+        // System.out.println("\nМассив после сортировки по времени: ");
+        // for (int i = 0; i < N; i++) {
+        // for (int j = 0; j < 6; j++) {
+        // System.out.print(LogData[i][j] + " ");
+        // }
+        // System.out.println();
+        // }
 
         int k = 0;
 
         for (int i = 0; i < N - 1; i++) {
-            System.out.println(i + " " + k + " " + countOfMinutes[k]);
-            if ((Integer.parseInt(LogData[i][3]) == Integer.parseInt(LogData[i + 1][3]))) {
-                if ((LogData[i][4].equals("A")) && ((LogData[i + 1][4].equals("B")) || (LogData[i + 1][4].equals("C")))) {
-                    countOfMinutes[k] += Integer.parseInt(LogData[i + 1][5]) - (Integer.parseInt(LogData[i][5]));
-                } else if (LogData[i][4].equals("B")) {
-                    countOfMinutes[k] += Integer.parseInt(LogData[i + 1][5]) - (Integer.parseInt(LogData[i][5]));
+            if (Integer.parseInt(LogData[i][3]) == Integer.parseInt(LogData[i + 1][3])) {
+                int minutesDifference = Integer.parseInt(LogData[i + 1][5]) - Integer.parseInt(LogData[i][5]);
+                String currentStatus = LogData[i][4];
+                String nextStatus = LogData[i + 1][4];
+
+                if ((currentStatus.equals("A") && (nextStatus.equals("B") || nextStatus.equals("C"))) ||
+                        currentStatus.equals("B")) {
+                    countOfMinutes[k] += minutesDifference;
                 }
-             } else {
+            } else {
                 k++;
                 countOfMinutes[k] = 0;
             }
@@ -134,7 +153,6 @@ public class App {
                 System.out.print(countOfMinutes[i] + " ");
             }
         }
-
 
     }
 }
